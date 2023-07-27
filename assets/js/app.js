ecf02b3591d16ae8df0f1bf6ebc0d2e26314e658
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2023-07-27 17:20:37 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -266,7 +266,7 @@ export function handleCalcHeight() {
 		const calcElmHeading = elmHeading.scrollHeight;
 		const elmBody = elmWrapper.querySelector('.handleCalcHeightBody');
 		const calcElmBody = elmBody.scrollHeight;
-		elmBody.style.height = calcElmBody + 'px';
+		elmBody.style.height = calcElmBody + 50 + 'px';
 		elmWrapper.style.height = calcElmHeading + calcElmBody + 'px';
 	}
 }
